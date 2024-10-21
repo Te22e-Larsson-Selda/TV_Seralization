5c1e0d9fcd4518e86617093b15a806bd8c829b39
Selda Larsson <selda.larssonsvensdotter@elev.ga.ntig.se>
2024-10-21 09:35:09 +0200

    1

---
+++ src/TVmain.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class TVmain {
+    private ArrayList<TVmain>
+    public TVmain() {}
+    public void Write2File(){}
+    public void readFromFrile(){}
+    public void creatData(){}
+
+}
---
+++ src/TvSeries.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public class TvSeries {
+    private String name;
+    ArrayList<>
+}
